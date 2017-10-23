025cb56a6f98c2c4baacba490f58b8552d9acff2
Zakhariy Stasenko <zakhariy.stasenko@globallogic.com>
2017-10-23 14:31:33 +0300

    Created base feature

---
+++ app/src/main/java/com/example/zakhariystasenko/retrophone/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.zakhariystasenko.retrophone;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int PERMISSION_REQUEST_CODE = 123;
+    private TextView mPhoneNumber;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+
+        initView();
+    }
+
+    private void initView(){
+        mPhoneNumber = (TextView) findViewById(R.id.phoneNumber);
+
+        findViewById(R.id.buttonCall).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tryCall();
+            }
+        });
+    }
+
+    private void tryCall(){
+        try {
+            makeCall();
+        } catch (SecurityException e){
+            requestCallPermission();
+        }
+    }
+
+    private void makeCall() throws SecurityException {
+        startActivity(new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + mPhoneNumber.getText())));
+    }
+
+    private void requestCallPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[]{Manifest.permission.CALL_PHONE}, PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERMISSION_REQUEST_CODE){
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                makeCall();
+            } else {
+                Toast.makeText(this, R.string.permission_toast, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
