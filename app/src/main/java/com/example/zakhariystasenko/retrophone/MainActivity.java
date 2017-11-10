7b6734c1f94f73e1dc63b0d2dc8adc3384ff77d8
Zakhariy Stasenko <zakhariy.stasenko@globallogic.com>
2017-11-10 16:58:28 +0200

    Reworked permissions

--- app/src/main/java/com/example/zakhariystasenko/retrophone/MainActivity.java
+++ app/src/main/java/com/example/zakhariystasenko/retrophone/MainActivity.java
@@ -1,58 +1,63 @@
 package com.example.zakhariystasenko.retrophone;
 
 import android.Manifest;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Color;
 import android.net.Uri;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
 
 import com.example.zakhariystasenko.retrophone.RetroPhoneView.RetroPhoneView;
 
 public class MainActivity extends AppCompatActivity implements RetroPhoneView.ActivityCallback {
     private static final int PERMISSION_REQUEST_CODE = 1;
-    private View mViewBlocker;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_layout);
-
-        ((RetroPhoneView) findViewById(R.id.phone)).setCallback(this);
-        mViewBlocker = findViewById(R.id.view_blocker);
-    }
 
     @Override
     protected void onStart() {
         super.onStart();
         if (ContextCompat.checkSelfPermission(this,
                 Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
-            mViewBlocker.setVisibility(View.GONE);
+            initMainView();
         } else {
-            requestPermissions(new String[]{Manifest.permission.CALL_PHONE}, PERMISSION_REQUEST_CODE);
+            initBlockView();
         }
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         if (grantResults.length != 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            mViewBlocker.setVisibility(View.GONE);
-        } else if (shouldShowRequestPermissionRationale(Manifest.permission.CALL_PHONE)) {
-            requestPermissions(new String[]{Manifest.permission.CALL_PHONE}, PERMISSION_REQUEST_CODE);
+            initMainView();
         }
     }
 
     @Override
     public void onNumberInputted(String phoneNumber) {
         startActivity(new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + phoneNumber)));
+    }
+
+    private void initMainView() {
+        setContentView(R.layout.main_layout);
+        ((RetroPhoneView) findViewById(R.id.phone)).setCallback(this);
+    }
+
+    private void initBlockView() {
+        setContentView(R.layout.view_blocker);
+        findViewById(R.id.permission_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (shouldShowRequestPermissionRationale(Manifest.permission.CALL_PHONE)) {
+                    requestPermissions(new String[]{Manifest.permission.CALL_PHONE}, PERMISSION_REQUEST_CODE);
+                } else {
+                    Intent intent = new Intent();
+                    intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                    intent.setData(Uri.fromParts("package", getPackageName(), null));
+                    startActivity(intent);
+                }
+            }
+        });
+        requestPermissions(new String[]{Manifest.permission.CALL_PHONE}, PERMISSION_REQUEST_CODE);
     }
 }
